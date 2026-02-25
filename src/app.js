328f9f5241dd77ff444e5898f499411dca4a133a
christ-leycene <christleycene@gmail.com>
2026-02-25 23:41:18 +0000

    Fix syntax and finalize app for tests

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-
 const tasksRouter = require('./routes/tasks');
 
-
 app.get('/', (req, res) => {
-
-  res.json({ message: "Welcome from FEATURE branch" };
-  
+    // Correction de la virgule et de la parenthèse ici
+    res.json({ message: "Task Manager API running (Lab2)" }); 
 });
-
-
 
 app.use('/tasks', tasksRouter);
 
-app.listen(3000, ()=> console.log("API running on port 3000"));
+
+if (require.main === module) {
+    app.listen(3000, () => console.log("API running on port 3000"));
+}
+
+module.exports = app; 
