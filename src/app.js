a0bdf70a7ba425300d29e46ffcc3422d1c1498e5
christ-leycene <christleycene@gmail.com>
2026-02-25 18:26:34 +0000

    Update welcome message on feature branch

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const tasksRouter = require('./routes/tasks');
 
 
 app.get('/', (req, res) => {
-  res.json({ message: "DevOps Task Manager API is running(Lab2)" });
+  res.json({ message: "Welcome from FEATURE branch" });
 });
 
 
