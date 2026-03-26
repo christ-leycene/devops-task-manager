d2b4e3aa85ede2c4a891155435c33b5323811063
christ-leycene <christleycene@gmail.com>
2026-03-26 23:30:43 +0100

    fix: add POST /tasks

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-const tasks = [
+// Données temporaires (en mémoire)
+let tasks = [
   { id: 1, title: "Learn Git", completed: false },
-  { id: 2, tille: "Practice DevOps", completed: true }
+  { id: 2, title: "Practice DevOps", completed: true }
 ];
 
-
+// GET /tasks
 router.get('/', (req, res) => {
   res.json(tasks);
 });
 
+// POST /tasks
+router.post('/', (req, res) => {
+  const { title, completed } = req.body;
+
+  // Vérification simple
+  if (!title) {
+    return res.status(400).json({ error: "Title is required" });
+  }
+
+  const newTask = {
+    id: tasks.length + 1,
+    title: title,
+    completed: completed || false
+  };
+
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
 
 module.exports = router;
-
